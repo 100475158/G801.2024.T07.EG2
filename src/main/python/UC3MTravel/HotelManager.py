a9b861517bb7680a794b9180001f4c1d3314c099
100475158 <100475158@alumnos.uc3m.es>
2024-03-12 17:39:47 +0100

    intro de la función validate credit card

--- src/main/python/UC3MTravel/HotelManager.py
+++ src/main/python/UC3MTravel/HotelManager.py
@@ -6,10 +6,36 @@ class HotelManager:
     def __init__(self):
         pass
 
-    def validatecreditcard( self, x ):
-        # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE GUID
-        # RETURN TRUE IF THE GUID IS RIGHT, OR FALSE IN OTHER CASE
-        return True
+    def validatecreditcard( self, numero_tarjeta ):
+        lista_tarjeta = []
+        # Cambio el numero de la tarjeta(que es un string) a una lista de enteros
+        # cambio el numero de tarjeta a string
+        for numero in str(numero_tarjeta):
+            lista_tarjeta.append(int(numero))
+        # Empiezo algoritmo de Luhn
+        # Invierto la lista para empezar por el ultimo numero pq el algoritmo lo requiere
+        lista_invertida = lista_tarjeta[::-1]
+        # Multiplicar por 2 cada segundo digito empezando por la derecha
+        # si el doble de un numero es mayor que 9 se suman las 2 cifras
+        for i in range(len(lista_invertida)):
+            if i % 2 != 0:
+                doble = lista_invertida[i] * 2
+                # si el doble de un numero es mayor que 9 se suman las 2 cifras
+                if doble > 9:
+                    cadena_doble = str(doble)
+                    suma_doble = 0
+                    for j in cadena_doble:
+                        suma_doble += int(j)
+                        lista_invertida[i] = suma_doble
+                else:
+                    lista_invertida[i] = doble
+        suma_digito = 0
+        for digito in lista_invertida:
+            suma_digito += digito
+        if suma_digito % 10 == 0:
+            return True
+        else:
+            return False
 
     def ReaddatafromJSOn( self, fi):
 
