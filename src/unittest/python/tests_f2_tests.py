0ca84dd35f4bcea47c7ae2354b9a14cc51c622a7
alvaromorenomartin <100472226@alumnos.uc3m.es>
2024-03-25 15:03:49 +0100

    Cambios unitest f2

--- src/unittest/python/tests_f2_tests.py
+++ src/unittest/python/tests_f2_tests.py
@@ -31,7 +31,7 @@ class TestGuestArrival(TestCase):
 
     def test_guest_arrival_test_tc2(self):
         for index, input_data in enumerate(self.__test_data_f2):
-            if index +1 in [2]:
+            if index +1 in [2,3,4,5,6,7,8,9,11,12,13,15,16,18,19,20,22,23,24,25,26,27,28,29,30,32,33,34,36,37,39,41,44,45,46,47,49,50,51,53,54,56,57,58,60]:
                 test_id = "TC" + str(index + 1)
                 with self.subTest(test_id):
                     print("Executing: "+ test_id + ":" +input_data)
@@ -40,4 +40,4 @@ class TestGuestArrival(TestCase):
                     room_key= hm.guest_arrival(self.__path_tests + self.tmp_test_data_file)
                     match test_id:
                         case "TC1":
-                            self.assertEqual(room_key, "")
+                            self.assertEqual(room_key, "El archivo JSON no tiene la estructura esperada")
