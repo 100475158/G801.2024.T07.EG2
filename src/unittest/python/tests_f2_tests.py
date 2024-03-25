96d43e6f3b519b0e875b52ab9845692f737635e2
alvaromorenomartin <100472226@alumnos.uc3m.es>
2024-03-25 17:31:15 +0100

    Cambios unitest f2 enfoque 2, por revisar

--- src/unittest/python/tests_f2_tests.py
+++ src/unittest/python/tests_f2_tests.py
@@ -6,10 +6,13 @@ from src.main.python.UC3MTravel.HotelReservation import HotelReservation
 import json
 import hashlib
 import datetime
+from pathlib import Path
+import os
+
 class TestGuestArrival(TestCase):
     def test_guest_arrival_test_ok(self):
         for index, input_data in enumerate(self.__test_data_f2):
-            if index +1 in [1]:
+            if index + 1 in [1]:
                 test_id = "TC" + str(index + 1)
                 with self.subTest(test_id):
                     print("Executing: "+ test_id + ":" +input_data)
@@ -18,6 +21,7 @@ class TestGuestArrival(TestCase):
                     room_key= hm.guest_arrival(self.__path_tests + self.tmp_test_data_file)
                     match test_id:
                         case "TC1":
+                            print(room_key)
                             self.assertEqual(room_key, "ee25b7b863b77e9106d851875103a3076748a0d487e7a42340ea18855d36b89f")
 
     def get_store_hash(self):
@@ -28,16 +32,29 @@ class TestGuestArrival(TestCase):
             file_hash = " "
         return file_hash
 
-
-    def test_guest_arrival_test_tc2(self):
+    def test_guest_arrival_test_invalid(self):
         for index, input_data in enumerate(self.__test_data_f2):
-            if index +1 in [2,3,4,5,6,7,8,9,11,12,13,15,16,18,19,20,22,23,24,25,26,27,28,29,30,32,33,34,36,37,39,41,44,45,46,47,49,50,51,53,54,56,57,58,60]:
+            if index +1 in range(2, 61):
                 test_id = "TC" + str(index + 1)
                 with self.subTest(test_id):
                     print("Executing: "+ test_id + ":" +input_data)
                     self.generate_tmp_test_data_file(input_data)
-                    hm = HotelManager()
-                    room_key= hm.guest_arrival(self.__path_tests + self.tmp_test_data_file)
-                    match test_id:
-                        case "TC1":
-                            self.assertEqual(room_key, "El archivo JSON no tiene la estructura esperada")
+                    with self.assertRaises(HotelManagementException) as ar:
+                        match test_id:
+                            case "TC1", "TC2", "TC3", "TC4", "TC5", "TC6", "TC7", "TC8", "TC9", "TC11","TC12","TC13","TC15","TC16","TC18","TC19","TC20","TC22","TC23","TC24","TC25","TC26","TC27","TC28","TC29","TC30","TC32","TC33","TC34","TC36","TC37","TC39","TC41","TC44","TC45","TC46","TC47","TC49","TC50","TC51","TC53","TC54","TC56","TC57","TC58","TC60":
+                                self.assertEqual(ar.exception.message, "El archivo JSON no tiene la estructura esperada")
+                            case "TC10":
+                                print(ar.exception.message)
+                                self.assertEqual(ar.exception.message, "No se encuentra el archivo de datos")
+                            case "TC14", "TC21", "TC35", "TC38", "TC42", "TC48", "TC55":
+                                print(ar.exception.message)
+                                self.assertEqual(ar.exception.message, "Los datos del JSON no tienen valores válidos")
+                            case "TC31", "TC52", "TC59":
+                                print(ar.exception.message)
+                                self.assertEqual(ar.exception.message, "El localizador no se corresponde con los datos almacenados")
+
+
+
+
+
+
