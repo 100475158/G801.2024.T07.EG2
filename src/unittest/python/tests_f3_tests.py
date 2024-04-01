8f76731bf33ba46ab869be85fe4eb932c50c8361
100475158 <100475158@alumnos.uc3m.es>
2024-04-02 00:31:16 +0200

    funciona otro test

--- src/unittest/python/tests_f3_tests.py
+++ src/unittest/python/tests_f3_tests.py
@@ -23,7 +23,7 @@ class TestGuestArrival(TestCase):
 
     @freeze_time("2024-10-16")
     def test_guest_checkout_test_tc2(self):
-        #Room key no es valido
+        # Room key no es valido
         checkout=HotelManager()
         with self.assertRaises(HotelManagementException) as cm:
             checkout.guest_checkout("770c547dc8adacd3860f47b8c9e8f6815269f967478a141e458aa4104dfab2")
@@ -47,7 +47,7 @@ class TestGuestArrival(TestCase):
         # No se encuentra el archivo hotel_stays.json
         checkout = HotelManager()
         with self.assertRaises(HotelManagementException) as cm:
-            checkout.guest_checkout("770c547dc8adacd3860f47b8c9e8f6815269f967478a141e458aa4104dfab24f")
+            checkout.guest_checkout("2f22c203b2765e4aa7c2308d3e41cdd64470bf9c701129ede7ddd34c3011063e")
         self.assertEqual(cm.exception.message, "No se encuentra el archivo de datos")
 
         # Restaura el contenido original del archivo después de completar el caso de prueba
@@ -69,18 +69,37 @@ class TestGuestArrival(TestCase):
         # La fecha de salida no coincide con la esperada
         checkout = HotelManager()
         with self.assertRaises(HotelManagementException) as cm:
-            checkout.guest_checkout("770c547dc8adacd3860f47b8c9e8f6815269f967478a141e458aa4104dfab24f")
+            checkout.guest_checkout("2f22c203b2765e4aa7c2308d3e41cdd64470bf9c701129ede7ddd34c3011063e")
         self.assertEqual(cm.exception.message,
                          "Error de procesamiento interno: La fecha de salida no es válida")
 
 
     @freeze_time("2024-10-16")
     def test_guest_checkout_test_tc6(self):
-        # No se encuentra el archivo registro_entregas.json
+        # No se encuentra el archivo check-outs.json
+        json_files_path = str(Path.home()) + "/PycharmProjects/G801.2024.T07.EG2/src/json_files/"
+        file_store = json_files_path + "check-outs.json"
+
+        # Guarda el contenido original del archivo
+        original_content = None
+        if os.path.isfile(file_store):
+            with open(file_store, "r") as f:
+                original_content = f.read()
+
+        # Elimina el archivo antes de ejecutar el caso de prueba
+        if os.path.isfile(file_store):
+            os.remove(file_store)
+
+        # No se encuentra el archivo hotel_stays.json
         checkout = HotelManager()
         with self.assertRaises(HotelManagementException) as cm:
-            checkout.guest_checkout("770c547dc8adacd3860f47b8c9e8f6815269f967478a141e458aa4104dfab24f")
-        self.assertEqual(cm.exception.message,"No se encuentra el archivo de datos")
+            checkout.guest_checkout("2f22c203b2765e4aa7c2308d3e41cdd64470bf9c701129ede7ddd34c3011063e")
+        self.assertEqual(cm.exception.message, "No se encuentra el archivo de datos")
+
+        # Restaura el contenido original del archivo después de completar el caso de prueba
+        if original_content is not None:
+            with open(file_store, "w") as f:
+                f.write(original_content)
 
     @freeze_time("2024-10-16")
     def test_guest_checkout_test_tc7(self):
